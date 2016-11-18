0622c1bc00bc0c0f4cf8ec69c141c6eaabb80e47
Michael Yanni <miyanni@microsoft.com>
2016-11-18 15:34:19 -0800

    Made it so that I can import Mirages from the Mirages.xml file.

--- WoFFTool/MainWindow.xaml.cs
+++ WoFFTool/MainWindow.xaml.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Xml;
 using System.Xml.Serialization;
 using WoFFTool.DomainObjects;
-using WoFFTool.ExportObjects;
 using WoFFTool.ImportObjects;
 
 namespace WoFFTool
@@ -51,12 +51,21 @@ namespace WoFFTool
             var names = resistanceItems.Select(r => r.Mirage).Union(prismtunityMementoItems.Select(pm => pm.Mirage));
             var mirages = names.Select(n => new Mirage(resistanceItems.SingleOrDefault(r => n == r.Mirage), prismtunityMementoItems.SingleOrDefault(pm => n == pm.Mirage))).ToList();
 
-            XmlSerializer serializer = new XmlSerializer(typeof(List<Mirage>));
+            var serializer = new XmlSerializer(typeof(List<Mirage>));
             using (var stringWriter = new StringWriter())
             using (var xmlWriter = XmlWriter.Create(stringWriter))
             {
                 serializer.Serialize(xmlWriter, mirages);
                 File.WriteAllText("Mirages.xml", stringWriter.ToString());
+            }
+        }
+
+        private void LoadMiragesBtn_Click(Object sender, RoutedEventArgs e)
+        {
+            var serializer = new XmlSerializer(typeof(List<Mirage>));
+            using(var reader = new StringReader(File.ReadAllText("Mirages.xml")))
+            {
+                var mirages = (List<Mirage>)serializer.Deserialize(reader);
             }
         }
     }
