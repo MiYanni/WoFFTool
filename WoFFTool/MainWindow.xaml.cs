1b0761703b2cfa641529dd4f0cb79c9a474d0fee
Michael Yanni <miyanni@microsoft.com>
2016-11-22 10:16:51 -0800

    Set up generic comparisons for filtering all the resistances.

--- WoFFTool/MainWindow.xaml.cs
+++ WoFFTool/MainWindow.xaml.cs
@@ -95,6 +95,25 @@ namespace WoFFTool
             Application.Current.Shutdown();
         }
 
+        private bool ResistanceFilter(string text, int? resistance, string comparer)
+        {
+            bool compare()
+            {
+                var value = Convert.ToInt32(text);
+                switch (comparer)
+                {
+                    case "=":
+                        return resistance == value;
+                    case ">=":
+                        return resistance >= value;
+                    case "<=":
+                        return resistance <= value;
+                }
+                return true;
+            }
+            return ((String.IsNullOrEmpty(text) || text.Equals("-")) || compare());
+        }
+
         private void UpdateFilteredMirages_TextChanged(Object sender, TextChangedEventArgs e)
         {
             FilteredMirages = Mirages.Where(m =>
@@ -102,23 +121,23 @@ namespace WoFFTool
                 (String.IsNullOrEmpty(MirageSizeFilterTxtBox.Text) || m.Size.GetName().ToLower().StartsWith(MirageSizeFilterTxtBox.Text.ToLower())) &&
                 ((String.IsNullOrEmpty(MirageWeightFilterTxtBox.Text) || MirageWeightFilterTxtBox.Text.Equals("-")) || m.Weight == Convert.ToInt32(MirageWeightFilterTxtBox.Text)) &&
 
-                ((String.IsNullOrEmpty(MirageFireFilterTxtBox.Text) || MirageFireFilterTxtBox.Text.Equals("-")) || m.Elemental.Fire == Convert.ToInt32(MirageFireFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageIceFilterTxtBox.Text) || MirageIceFilterTxtBox.Text.Equals("-")) || m.Elemental.Ice == Convert.ToInt32(MirageIceFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageThunderFilterTxtBox.Text) || MirageThunderFilterTxtBox.Text.Equals("-")) || m.Elemental.Thunder == Convert.ToInt32(MirageThunderFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageAeroFilterTxtBox.Text) || MirageAeroFilterTxtBox.Text.Equals("-")) || m.Elemental.Aero == Convert.ToInt32(MirageAeroFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageWaterFilterTxtBox.Text) || MirageWaterFilterTxtBox.Text.Equals("-")) || m.Elemental.Water == Convert.ToInt32(MirageWaterFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageEarthFilterTxtBox.Text) || MirageEarthFilterTxtBox.Text.Equals("-")) || m.Elemental.Earth == Convert.ToInt32(MirageEarthFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageLightFilterTxtBox.Text) || MirageLightFilterTxtBox.Text.Equals("-")) || m.Elemental.Light == Convert.ToInt32(MirageLightFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageDarkFilterTxtBox.Text) || MirageDarkFilterTxtBox.Text.Equals("-")) || m.Elemental.Dark == Convert.ToInt32(MirageDarkFilterTxtBox.Text)) &&
+                ResistanceFilter(MirageFireFilterTxtBox.Text, m.Elemental.Fire, "=") &&
+                ResistanceFilter(MirageIceFilterTxtBox.Text, m.Elemental.Ice, "=") &&
+                ResistanceFilter(MirageThunderFilterTxtBox.Text, m.Elemental.Thunder, "=") &&
+                ResistanceFilter(MirageAeroFilterTxtBox.Text, m.Elemental.Aero, "=") &&
+                ResistanceFilter(MirageWaterFilterTxtBox.Text, m.Elemental.Water, "=") &&
+                ResistanceFilter(MirageEarthFilterTxtBox.Text, m.Elemental.Earth, "=") &&
+                ResistanceFilter(MirageLightFilterTxtBox.Text, m.Elemental.Light, "=") &&
+                ResistanceFilter(MirageDarkFilterTxtBox.Text, m.Elemental.Dark, "=") &&
 
-                ((String.IsNullOrEmpty(MiragePoisonFilterTxtBox.Text) || MiragePoisonFilterTxtBox.Text.Equals("-")) || m.Ailment.Poison == Convert.ToInt32(MiragePoisonFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageConfuseFilterTxtBox.Text) || MirageConfuseFilterTxtBox.Text.Equals("-")) || m.Ailment.Confuse == Convert.ToInt32(MirageConfuseFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageSleepFilterTxtBox.Text) || MirageSleepFilterTxtBox.Text.Equals("-")) || m.Ailment.Sleep == Convert.ToInt32(MirageSleepFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageBlindFilterTxtBox.Text) || MirageBlindFilterTxtBox.Text.Equals("-")) || m.Ailment.Blind == Convert.ToInt32(MirageBlindFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageOblivionFilterTxtBox.Text) || MirageOblivionFilterTxtBox.Text.Equals("-")) || m.Ailment.Oblivion == Convert.ToInt32(MirageOblivionFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageBerserkFilterTxtBox.Text) || MirageBerserkFilterTxtBox.Text.Equals("-")) || m.Ailment.Berserk == Convert.ToInt32(MirageBerserkFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageSlowFilterTxtBox.Text) || MirageSlowFilterTxtBox.Text.Equals("-")) || m.Ailment.Slow == Convert.ToInt32(MirageSlowFilterTxtBox.Text)) &&
-                ((String.IsNullOrEmpty(MirageDoomFilterTxtBox.Text) || MirageDoomFilterTxtBox.Text.Equals("-")) || m.Ailment.Doom == Convert.ToInt32(MirageDoomFilterTxtBox.Text)) &&
+                ResistanceFilter(MiragePoisonFilterTxtBox.Text, m.Ailment.Poison, "=") &&
+                ResistanceFilter(MirageConfuseFilterTxtBox.Text, m.Ailment.Confuse, "=") &&
+                ResistanceFilter(MirageSleepFilterTxtBox.Text, m.Ailment.Sleep, "=") &&
+                ResistanceFilter(MirageBlindFilterTxtBox.Text, m.Ailment.Blind, "=") &&
+                ResistanceFilter(MirageOblivionFilterTxtBox.Text, m.Ailment.Oblivion, "=") &&
+                ResistanceFilter(MirageBerserkFilterTxtBox.Text, m.Ailment.Berserk, "=") &&
+                ResistanceFilter(MirageSlowFilterTxtBox.Text, m.Ailment.Slow, "=") &&
+                ResistanceFilter(MirageDoomFilterTxtBox.Text, m.Ailment.Doom, "=") &&
 
                 (String.IsNullOrEmpty(MiragePrismtunityFilterTxtBox.Text) || m.Prismtunity.ToLower().Contains(MiragePrismtunityFilterTxtBox.Text.ToLower())) &&
                 (String.IsNullOrEmpty(MirageMementoLocationFilterTxtBox.Text) || m.MementoLocation.ToLower().Contains(MirageMementoLocationFilterTxtBox.Text.ToLower()))
@@ -128,6 +147,7 @@ namespace WoFFTool
 
         private void DigitsOnly_PreviewTextInput(Object sender, TextCompositionEventArgs e)
         {
+            // TODO: Change to only 1 '-' at the start.
             var regex = new Regex("^-?[0-9]*$");
             e.Handled = !regex.IsMatch(e.Text);
         }
